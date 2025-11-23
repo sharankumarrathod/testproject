e840e34196ab4df7d550b6e3fcbc0e5bfb167386
sharankumarrathod <115835863+sharankumarrathod@users.noreply.github.com>
2025-11-23 20:27:52 +0530

    i update the new class called ma that include that state and capital

--- githubNewProject/src/githubNewProject/JustPrint.java
+++ githubNewProject/src/githubNewProject/JustPrint.java
@@ -4,6 +4,6 @@ public class JustPrint {
 
 	public static void main(String[] args) {
 		System.out.println("hello ");
-		System.out.prinln("i am changing from git only ");
+		System.out.println("i am changing from git only ");
 	}
 }
---
+++ githubNewProject/src/githubNewProject/MapEntryChecking.java
@@ -0,0 +1,21 @@
+package githubNewProject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapEntryChecking {
+	public static void main(String[] args) {
+		
+		Map<String,String > StateCapital = new HashMap<String, String>();
+		StateCapital.put("karantak", "benagaluru");
+		StateCapital.put("maharatstra", "mubai");
+		StateCapital.put("kerala", "kochi");
+		StateCapital.put("telagana", "hydrabad");
+		StateCapital.put("telagana", "amravati");
+		StateCapital.put("telagana", "secundrabad");
+		
+		System.out.println(StateCapital.keySet()+"    "+StateCapital.values());
+		
+	}
+
+}
